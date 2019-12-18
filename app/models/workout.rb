5cfc7dc83cba0385e383160f259676a4335fb591
Edmunds <edmunds.korsaks@gmail.com>
2019-12-18 09:20:45 +0200

    RSpec test

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,6 @@
 class Workout < ApplicationRecord
 	has_many :exercises, dependent: :destroy
+	validates :workout, presence: true 
+	validates :mood, presence: true
+	validates :length, presence: true
 end
