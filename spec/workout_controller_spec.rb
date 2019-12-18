c434b5a2ea7e72d766e71c8069a5ac247915a97d
Edmunds <edmunds.korsaks@gmail.com>
2019-12-18 09:39:24 +0200

    RSpec test

--- spec/workout_controller_spec.rb
+++ spec/workout_controller_spec.rb
@@ -2,14 +2,14 @@ require './rails_helper'
 
 RSpec.describe WorkoutsController, type: :controller do
 	context 'GET #index' do
-		it 'returns a success response' do
+		it 'should returns a success response' do
 			get :index
 			expect(response).to be_success
 		end
 	end
 
-	context 'GET #show' do
-		it 'returns a success response' do
+	context 'GET #now' do
+		it 'should returns a success response' do
 			workout = Workout.create!(workout: 'running', mood: 'lazy', length: '60min')
 			get :new, params: { id: workout.to_param }
 			expect(response).to be_success
